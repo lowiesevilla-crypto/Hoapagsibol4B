32d1ccdd25dbef6d54aef181a07e36b4e13ce71d
lowiesevilla-crypto <lowie.sevilla@gmail.com>
2026-08-24 12:06:50 +0800

    docs(payroll): tag report print control

--- components/payroll-report-print-button.tsx
+++ components/payroll-report-print-button.tsx
@@ -2,6 +2,10 @@
 
 import { Printer } from "lucide-react";
 
+/**
+ * @requirement PAY-RPT-001
+ * @status IMPLEMENTED
+ */
 export function PayrollReportPrintButton() {
   return <button className="btn-secondary print:hidden" type="button" onClick={() => window.print()}>
     <Printer className="size-4" /> Print report
